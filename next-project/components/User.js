a15c58182ac2fce17f83bef7cb83aaed030a2b4f
zl95826 <zl95826@hotmail.com>
2020-06-05 22:40:00 -0700

    getStaticProps

--- next-project/components/User.js
+++ next-project/components/User.js
@@ -3,6 +3,11 @@ const user=({name,age})=>(
     <div>
         <h1>{name}</h1>
         <p>Age: {age}</p>
+<style jsx>{`
+div {border:1px solid #eee;
+     box-shadow:0 2px 3px #ccc;
+    padding:30px;text-align:center;}
+`}</style>
     </div>
 
 );
--- next-project/pages/auth/index.js
+++ next-project/pages/auth/index.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import User from '../../components/User';
 
-const authIndexPage=()=>(
+const authIndexPage=({posts})=>(
     <div>
-        <h1>The Auth Index Page</h1>
+        <h1>The Auth Index Page:<span className='auth'> {posts.title}</span></h1>
         <User name='Lee' age={22} />
+<style jsx>{
+    `.auth{color:red;}
+    `
+}</style>
     </div>
 );
+// const authIndexPage=(props)=>(
+//     <div>
+//         <h1>The Auth Index Page: {props.posts.title}</h1>
+//         <User name='Lee' age={22} />
+//     </div>
+// );
+export async function getStaticProps() {
+    const res=await fetch('https://jsonplaceholder.typicode.com/todos/1')
+    const posts=await res.json();
+    return {props:{posts}}
+}
 
 export default authIndexPage;
